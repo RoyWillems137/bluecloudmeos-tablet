3312c204ada08cbcb29ef13d3beab895ff116050
Roy Willems <93043115+RoyWillems137@users.noreply.github.com>
2025-07-07 15:41:30 +0200

    Update script.js

--- html/js/script.js
+++ html/js/script.js
@@ -1,28 +1,37 @@
-OpenPoliceTablet = function() {
+OpenPoliceTablet = function () {
     TabletOpen = true;
     $('.police-tablet').show(250);
 }
 
-CloseTablet = function() {
+CloseTablet = function () {
     TabletOpen = false;
     $('.police-tablet').hide(250);
     $.post(`https://${GetParentResourceName()}/CloseTablet`, JSON.stringify({}))
 }
 
 $(function () {
-    window.addEventListener('message', function(event) {
-        switch(event.data.action) {
+    window.addEventListener('message', function (event) {
+        switch (event.data.action) {
             case "OpenPoliceTablet":
                 OpenPoliceTablet()
                 break;
         }
     });
-    window.onkeydown = function(e) {
+    window.onkeydown = function (e) {
         var e = e || window.event;
         if (e.keyCode === 27) {
             CloseTablet();
         }
     };
+
+    window.addEventListener('message', function (event) {
+        if (event.data.action === "reload") {
+            const iframe = document.querySelector("iframe");
+            if (iframe) {
+                iframe.src = iframe.src;
+            }
+        }
+    });
 });
 
 
