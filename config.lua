fc56552442f4c8ede0604365a0a95c2008475858
Roy Willems <93043115+RoyWillems137@users.noreply.github.com>
2025-07-07 15:39:30 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,5 @@
 Config = {
     ["EnableAnimations"] = true, -- Wil je tablet animaties in of uitgeschakeld hebben
     ["command"] = "meos", -- Welk command moet een speler typen om het Meos te mogen gebruiken
+    ["resetcommand"] = "resetmeos", -- Welk command moet een speler typen om het Meos te kunnen resetten wanneer deze vastloopt
 }
